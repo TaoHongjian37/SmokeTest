2053bd13c2012aa1068a35088b203e794ef8ada9
THJZNB <2562670127@qq.com>
2025-11-15 20:39:50 +0800

    Initial Commit

---
+++ Smoke test/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Smoke test
+//
+//  Created by Tao on 2025/11/15.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Smoke test/Smoke_testApp.swift
@@ -0,0 +1,34 @@
+//
+//  Smoke_testApp.swift
+//  Smoke test
+//
+//  Created by Tao on 2025/11/15.
+//
+
+import SwiftUI
+
+@main
+struct Smoke_testApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
