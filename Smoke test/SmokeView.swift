9c5bd36b8b0d2a1fe1b661d404a00c5d9ebb793a
THJZNB <2562670127@qq.com>
2025-11-18 15:38:36 +0800

    在 TaoTest 分支新增 SmokeView/SwiftUIView 并调整 VerticalBillboard

---
+++ Smoke test/SmokeView.swift
@@ -0,0 +1,18 @@
+//
+//  SmokeView.swift
+//  Smoke test
+//
+//  Created by Tao on 2025/11/18.
+//
+
+import SwiftUI
+
+struct SmokeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmokeView()
+}
